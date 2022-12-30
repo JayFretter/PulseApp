6afdbc3ab8c1fb577f3c56c16ad00def790d0138
Jay Fretter <jhfretter23@gmail.com>
2022-12-30 16:34:59 +0000

    Added Tailwind to project

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -5,8 +5,8 @@ function ErrorPage() {
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
+    <div className="flex flex-col items-center justify-center w-full text-xl">
+      <h1 className="text-3xl font-bold">Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
         <i>{error.statusText || error.message}</i>
