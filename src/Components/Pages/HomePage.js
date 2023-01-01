bf410fbf42eac66fc27c0e3c9d344ab4a6cc4d5b
Jay Fretter <jhfretter23@gmail.com>
2023-01-01 19:35:56 +0000

    Created base home layout for pulse cards

--- src/Components/Pages/HomePage.js
+++ src/Components/Pages/HomePage.js
@@ -1,11 +1,17 @@
 function HomePage() {
   return (
-    <div>
-      <header className="min-h-screen flex flex-col justify-center items-center bg-slate-900 text-4xl text-white">
-        <p>
-          Welcome to Pulse!
-        </p>
-      </header>
+    <div className="min-h-screen flex flex-col items-center bg-slate-900 text-white">
+      <p className="my-8 text-4xl">Welcome to Pulse!</p>
+      <div className="w-full px-10 flex flex-col lg:flex-row lg:justify-center gap-8 lg:gap-40 text-center text-slate-100">
+        <div className="pulse-card">
+          <p className="mt-2">Lorum ipsum dolor sit amet?</p>
+          <div className="w-[240px] h-[240px] mt-8 bg-purple-600 rounded-full mb-2"></div>
+        </div>
+        <div className="pulse-card">
+          <p className="mt-2">Lorum ipsum dolor sit amet?</p>
+          <div className="w-[240px] h-[240px] mt-8 bg-purple-600 rounded-full mb-2"></div>
+        </div>
+      </div>
     </div>
   );
 }
