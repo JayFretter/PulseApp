605ccbf525c4c01d29f079a8b6f522259b5288d7
Jay Fretter <jhfretter23@gmail.com>
2023-01-01 19:46:18 +0000

    Now hiding nav links on small devices

--- src/Components/Pages/HomePage.js
+++ src/Components/Pages/HomePage.js
@@ -1,13 +1,14 @@
 function HomePage() {
   return (
     <div className="min-h-screen flex flex-col items-center bg-slate-900 text-white">
-      <p className="my-8 text-4xl">Welcome to Pulse!</p>
+      <p className="mt-8 mb-2 text-4xl">Welcome to Pulse.</p>
+      <p className="text-lg font-light mb-12">Check out todays trending topics:</p>
       <div className="w-full px-10 flex flex-col lg:flex-row lg:justify-center gap-8 lg:gap-40 text-center text-slate-100">
-        <div className="pulse-card">
+        <div className="pulse-card lg:text-2xl">
           <p className="mt-2">Lorum ipsum dolor sit amet?</p>
           <div className="w-[240px] h-[240px] mt-8 bg-purple-600 rounded-full mb-2"></div>
         </div>
-        <div className="pulse-card">
+        <div className="pulse-card lg:text-2xl">
           <p className="mt-2">Lorum ipsum dolor sit amet?</p>
           <div className="w-[240px] h-[240px] mt-8 bg-purple-600 rounded-full mb-2"></div>
         </div>
--- src/Components/Shared/Navbar.js
+++ src/Components/Shared/Navbar.js
@@ -10,15 +10,15 @@ function Navbar() {
     {
       return (
         <div className="flex items-center gap-4 uppercase font-thin">
-          <Link to={`/`}>Log Out</Link>
-          <Link to={`profile`}>Profile</Link>
+          <Link to={`/`} className="hidden md:block">Log Out</Link>
+          <Link to={`profile`} className="hidden md:block">Profile</Link>
         </div>
       )
     }
 
     return (
       <div className="flex items-center gap-4 uppercase font-thin">
-        <Link to={`/login`}>Log In</Link>
+        <Link to={`/login`} className="hidden md:block">Log In</Link>
       </div>
     )
   }
@@ -26,7 +26,7 @@ function Navbar() {
   return (
     <div className="flex items-center justify-between px-8 h-[60px] bg-purple-800 text-white">
       <Link to={`/`} className="font-semibold text-3xl tracking-wide hover:cursor-pointer">pulse.</Link>
-      <input type="text" placeholder="Search for a pulse..." className="w-[300px] h-[50%] rounded-lg bg-purple-200 p-2 text-black" />
+      <input type="text" placeholder="Search for a pulse..." className="max-w-[80%] min-w-[220px] h-[50%] rounded-lg bg-purple-200 p-2 text-black" />
       {getNavbarLinks()}
     </div>
   );
